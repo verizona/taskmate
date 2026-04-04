b49cb5a482a9fcf0e022aa3a92ac7870bd0932f3
verizona <127630011+verizona@users.noreply.github.com>
2026-04-03 20:31:02 -0400

    Create schema.sql

---
+++ database/schema.sql
@@ -0,0 +1,56 @@
+-- WARNING: This schema is for context only and is not meant to be run.
+-- Table order and constraints may not be valid for execution.
+
+CREATE TABLE public.list_members (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  list_id uuid,
+  user_id uuid NOT NULL,
+  role text NOT NULL CHECK (role = ANY (ARRAY['owner'::text, 'editor'::text, 'member'::text])),
+  CONSTRAINT list_members_pkey PRIMARY KEY (id),
+  CONSTRAINT list_members_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.profiles(id),
+  CONSTRAINT list_members_list_id_fkey FOREIGN KEY (list_id) REFERENCES public.lists(id)
+);
+CREATE TABLE public.lists (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  name text,
+  owner_id uuid,
+  created_at timestamp with time zone DEFAULT now(),
+  CONSTRAINT lists_pkey PRIMARY KEY (id),
+  CONSTRAINT lists_owner_id_fkey FOREIGN KEY (owner_id) REFERENCES auth.users(id)
+);
+CREATE TABLE public.profiles (
+  id uuid NOT NULL,
+  email text NOT NULL UNIQUE,
+  full_name text,
+  avatar_url text,
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  CONSTRAINT profiles_pkey PRIMARY KEY (id),
+  CONSTRAINT profiles_id_fkey FOREIGN KEY (id) REFERENCES auth.users(id)
+);
+CREATE TABLE public.task_lists (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  owner_user_id uuid NOT NULL,
+  name text NOT NULL,
+  is_personal boolean NOT NULL DEFAULT false,
+  CONSTRAINT task_lists_pkey PRIMARY KEY (id),
+  CONSTRAINT task_lists_owner_user_id_fkey FOREIGN KEY (owner_user_id) REFERENCES auth.users(id)
+);
+CREATE TABLE public.tasks (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  created_at timestamp with time zone NOT NULL DEFAULT now(),
+  user_id uuid,
+  title text DEFAULT 'text'::text,
+  is_completed boolean DEFAULT false,
+  due_date date,
+  priority text,
+  list_id uuid NOT NULL,
+  is_complete boolean NOT NULL DEFAULT false,
+  reminder_minutes integer,
+  notes text,
+  due_time text,
+  completed_at timestamp with time zone,
+  CONSTRAINT tasks_pkey PRIMARY KEY (id),
+  CONSTRAINT tasks_list_id_fkey FOREIGN KEY (list_id) REFERENCES public.lists(id)
+);
